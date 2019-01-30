2142d4058097c1270df962eb3dde9183fc501c35
thomasmartensgithub <thomasmartensgithub@users.noreply.github.com>
2019-01-30 21:39:48 +0100

    Add files via upload

---
+++ arduino_vs_receive.ino
@@ -0,0 +1,19 @@
+int led = 13;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available())
+    {
+      int data = Serial.read();
+      if(data == 'A')
+        digitalWrite(led,HIGH);
+      if(data == 'B')
+        digitalWrite(led,LOW); 
+    }
+}
+
